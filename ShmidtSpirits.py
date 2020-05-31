9a025dc5b7fd28991fa0134e6bacc94b993633fb
arthur <arthur@tartarus>
2020-05-30 22:18:49 -0400

    Updated integer to float

--- ShmidtSpirits.py
+++ ShmidtSpirits.py
@@ -20,13 +20,13 @@ class database:
 
     def addTable(self):
         d = datetime.datetime.now()
-        new_table = "CREATE TABLE D" + d.strftime("%y%m%d") + " (Time VARCHAR(10), Temp INTEGER(8), ID INTEGER AUTO_INCREMENT PRIMARY KEY)"
+        new_table = "CREATE TABLE D" + d.strftime("%y%m%d") + " (ID INTEGER AUTO_INCREMENT PRIMARY KEY, Time VARCHAR(10), Supply_STPT INTEGER(8), Supply_Temp FLOAT(8), Deph_Ret_STPT INTEGER(8), Deph_Ret_Temp FLOAT(8))"
         self.C.execute(new_table)
 
-    def addData(self,temperature):
+    def addData(self, Supply_STPT, Supply_Temp, Deph_Ret_STPT, Deph_Ret_Temp):
         d = datetime.datetime.now()
-        sql = "INSERT INTO D" + d.strftime("%y%m%d") + " (Time, Temp) VALUES (%s, %s)"
-        val = (d.strftime("%X"),temperature)
+        sql = "INSERT INTO D" + d.strftime("%y%m%d") + " (Time, Supply_STPT, Supply_Temp, Deph_Ret_STPT, Deph_Ret_Temp) VALUES (%s, %s, %s, %s, %s)"
+        val = (d.strftime("%X"),Supply_STPT, Supply_Temp, Deph_Ret_STPT, Deph_Ret_Temp)
         self.C.execute(sql,val)
         self.conn.commit()
 
@@ -43,7 +43,7 @@ if __name__ == "__main__":
     counter = 0
     for x in range(60):
         counter = x + randint(0,10)
-        data.addData(counter)
+        data.addData(90,counter+0.1,140,counter+50+0.1)
         print("X: {}, temp {}".format(x,counter))
         time.sleep(1)
 
